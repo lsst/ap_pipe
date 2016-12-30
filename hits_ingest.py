2669d5ca1fada5a9f73557c8b5a76d5759d85198
Meredith Rawls <mrawls@uw.edu>
2016-12-30 12:59:40 -0800

    Add script to ingest HiTS data

---
+++ hits_ingest.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+#from astropy.io import fits
+from lsst.utils import getPackageDir
+from lsst.obs.decam import ingest, ingestCalibs
+from lsst.pipe.tasks.ingest import IngestConfig
+from glob import glob
+import sys
+from lsst.obs.decam.ingest import DecamParseTask
+'''
+Little script to ingest some raw decam images
+
+You have to specify two things:
+1. repodir, an empty directory where you want the nicely organized and ingested images to live
+2. datadir, a directory where some '*.fits.fz' images currently live
+
+Usage: $ python hits_ingest.py 'path/to/datadir/'
+
+Result: repodir will be populated with **links** to the files in datadir, organized by date
+
+The command line equivalent given the variables below is:
+$ ingestImagesDecam.py repodir datafiles --filetype raw --mode link
+'''
+
+repodir = 'ingested/' #'../decam_hits/repo/'
+#datadir = 'HiTS/Blind15A_38/' #'../decam_hits/data/'
+datadir = sys.argv[1]
+datafiles = glob(datadir + '*.fits.fz')
+
+# first, need to make a text file that handles the mapper, per the obs_decam github README
+f = open(repodir+'_mapper', 'w')
+print('lsst.obs.decam.DecamMapper', file=f)
+f.close()
+
+# next, create the arguments you'd usually put on the command line after 'ingestImagesDecam.py'
+# (extend the list with all the filenames as the last set of arguments)
+args = [repodir, '--filetype', 'raw', '--mode', 'link']
+args.extend(datafiles)
+
+# set up the decam ingest task so it can take arguments ('name' says which file in obs_decam/config to use)
+argumentParser = ingest.DecamIngestArgumentParser(name='ingest')
+
+# create an instance of ingest configuration
+config = IngestConfig()
+config.parse.retarget(DecamParseTask) # this is from line 2 of obs_decam/config/ingest.py 
+
+# create an instance of the decam ingest task
+ingestTask = ingest.DecamIngestTask(config=config)
+
+# feed everything to the argument parser
+parsedCmd = argumentParser.parse_args(config=config, args=args)
+
+# finally, run the ingestTask
+ingestTask.run(parsedCmd)
+
