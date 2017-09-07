1b42cfa18f58044c8cd5f68ee431b6160d1a64fc
Eric Bellm <ecbellm@uw.edu>
2017-09-07 15:29:48 -0500

    make both PsfMatched and Direct coadds

--- config/makeCoaddTempExp_goodSeeing.py
+++ config/makeCoaddTempExp_goodSeeing.py
@@ -7,16 +7,8 @@ config.bgSubtracted=True
 #config.coaddName='goodSeeing'
 
 config.select.retarget(MaxPsfWcsSelectImagesTask)
-config.doPsfMatch=True
-# !! This must be the same as the kernelSize in the processEimage psfMeasurement.
+config.makePsfMatched=True
+config.makeDirect=True
+# !! This must be the same as the kernelSize in the processEimage
+# psfMeasurement.
 config.modelPsf.size=25
-# I don't know why the wings hurt the matching so much, but I couldn't get matching to
-# work in many cases with Run 3 data unless I turned this off.
-config.modelPsf.addWing=False
-config.warpAndPsfMatch.matchThenWarp=True
-# Size (rows) in pixels of each SpatialCell for spatial modeling
-config.warpAndPsfMatch.psfMatch.kernel['AL'].sizeCellX=500
-
-# Size (columns) in pixels of each SpatialCell for spatial modeling
-config.warpAndPsfMatch.psfMatch.kernel['AL'].sizeCellY=500
-
