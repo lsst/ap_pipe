8b3066594b236422c8404f30f523c8256cd3c8a2
Eric Bellm <ecbellm@uw.edu>
2018-05-30 12:11:02 -0500

    Remove commented out parameters.

--- config/makeCoaddTempExp_goodSeeing.py
+++ config/makeCoaddTempExp_goodSeeing.py
@@ -1,7 +1,6 @@
 from lsst.pipe.tasks.selectImages import BestSeeingWcsSelectImagesTask
 
 config.bgSubtracted = True
-#config.coaddName='goodSeeing'
 config.coaddName='deep'
 config.select.retarget(BestSeeingWcsSelectImagesTask)
 config.makePsfMatched = True
