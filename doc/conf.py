1dbd8c2e017529ad0410b905a587638f0c960267
Jonathan Sick <jsick@lsst.org>
2021-05-04 16:06:32 -0400

    Update conf.py for documenteer 0.6
    Update conf.py for the documenteer 0.6 Sphinx configuration API

--- doc/conf.py
+++ doc/conf.py
@@ -1,12 +1,13 @@
 """Sphinx configuration file for an LSST stack package.
-
-This configuration only affects single-package Sphinx documenation builds.
+This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
-from documenteer.sphinxconfig.stackconf import build_package_configs
-import lsst.ap.pipe
+from documenteer.conf.pipelinespkg import *
 
-_g = globals()
-_g.update(build_package_configs(
-    project_name='ap_pipe',
-    version=lsst.ap.pipe.version.__version__))
+
+project = "ap_pipe"
+html_theme_options["logotext"] = project
+html_title = project
+html_short_title = project
