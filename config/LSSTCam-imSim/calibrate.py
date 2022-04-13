aa80791509a9ceae9ef1d6f5a63ffaf0ee0496a7
Krzysztof Findeisen <kfindeis@uw.edu>
2022-04-12 19:30:43 -0500

    Decouple calibrate.py from obs_lsst.

--- config/LSSTCam-imSim/calibrate.py
+++ config/LSSTCam-imSim/calibrate.py
@@ -31,6 +31,9 @@ LSST Cam-specific overrides for CalibrateTask
 
 import os.path
 
+# HACK: Throw away any changes imposed by obs configs, especially plugins.
+config = type(config)()
+
 obsConfigDir = os.path.join(os.path.dirname(__file__))
 
 bgFile = os.path.join(obsConfigDir, "background.py")
