83795391f56e6e938bc2c2529347024d7b4b6007
Eli Rykoff <erykoff@stanford.edu>
2022-04-13 14:07:48 -0700

    Remove unused gen2 config file.

--- config/makeCoaddTempExp_goodSeeing.py
+++
@@ -1,7 +0,0 @@
-from lsst.pipe.tasks.selectImages import BestSeeingWcsSelectImagesTask
-
-config.bgSubtracted = True
-config.coaddName='deep'
-config.select.retarget(BestSeeingWcsSelectImagesTask)
-config.makePsfMatched = True
-config.makeDirect = True
