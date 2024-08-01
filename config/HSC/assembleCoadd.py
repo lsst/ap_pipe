d45d5e80f81d5c77d4d2bcfc6b4d6f60bbf8edb0
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2024-08-01 00:01:57 -0400

    Remove coaddBase and matchingKernelSize

--- config/HSC/assembleCoadd.py
+++ config/HSC/assembleCoadd.py
@@ -3,13 +3,8 @@
 # permanently remove the obs_* configs, we should check that none of the
 # changes made there since April 12, 2022 would be useful here.
 
-import os.path
-
 # HACK: Throw away any changes imposed by obs configs.
 config.loadFromString(type(config)().saveToString())
-
-# Load configs shared between assembleCoadd and makeCoaddTempExp
-config.load(os.path.join(os.path.dirname(__file__), "coaddBase.py"))
 
 # 200 rows (since patch width is typically < 10k pixels)
 config.subregionSize = (10000, 200)
@@ -21,7 +16,6 @@ config.doAttachTransmissionCurve = True
 # Saturation trails are usually oriented east-west, so along rows
 config.interpImage.transpose = True
 config.coaddPsf.warpingKernelName = 'lanczos5'
-config.matchingKernelSize = 29
 
 from lsst.pipe.tasks.selectImages import PsfWcsSelectImagesTask
 config.select.retarget(PsfWcsSelectImagesTask)
