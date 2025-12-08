53e774fb5d7e7df0787da138a0eb76a02b7b3100
Erin Howard <elhoward@uw.edu>
2025-12-08 11:56:23 -0800

    Add template pipeline using Prompt PVIs as input.

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -61,6 +61,10 @@ class PipelineDefintionsTestSuite(lsst.utils.tests.TestCase):
                 # Our QuickTemplate definition cannot be tested here because it
                 # depends on drp_tasks, which we cannot make a dependency here.
                 continue
+            if "PromptTemplate" in file:
+                # Our PromptTemplate definition cannot be tested here because it
+                # depends on drp_tasks, which we cannot make a dependency here.
+                continue
             with self.subTest(file):
                 pipeline = lsst.pipe.base.Pipeline.from_uri(file)
                 pipeline.addConfigOverride("parameters", "apdb_config", "some/file/path.yaml")
@@ -120,6 +124,10 @@ class PipelineDefintionsTestSuite(lsst.utils.tests.TestCase):
             elif "ApdbDeduplication" in file:
                 # The task to export catalogs from the APDB and re-run
                 # association is not intended to be part of
+                continue
+            elif "PromptTemplate" in file:
+                # Our PromptTemplate definition cannot be tested here because it
+                # depends on drp_tasks, which we cannot make a dependency here.
                 continue
             with self.subTest(file):
                 pipeline = lsst.pipe.base.Pipeline.from_uri(file)
