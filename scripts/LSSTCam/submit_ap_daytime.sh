7e41ed81f4ad16f1b1b04ef379557156a3afa24a
Ian Sullivan <sullii@uw.edu>
2026-01-21 16:19:21 -0800

    Remove template constraint from Daytime graph

--- scripts/LSSTCam/submit_ap_daytime.sh
+++ scripts/LSSTCam/submit_ap_daytime.sh
@@ -59,7 +59,7 @@ BAD_DETECTORS_SQL="($(printf '%s,' $BAD_DETECTORS | sed 's/,$//'))"
 BLOCKS_SQL="($(printf "'%s'," $BLOCKS | sed 's/,$//'))"
 
 nohup bps submit "${AP_PIPE_DIR}/bps/LSSTCam/bps_Daytime.yaml" \
-  --extra-qgraph-options "--skip-existing-in LSSTCam/prompt/output-${DATE} -c parameters:release_id=1 -c parameters:apdb_config=${TMP_APDB} --dataset-query-constraint template_coadd" \
+  --extra-qgraph-options "--skip-existing-in LSSTCam/prompt/output-${DATE} -c parameters:release_id=1 -c parameters:apdb_config=${TMP_APDB} --dataset-query-constraint off" \
   --extra-run-quantum-options "--no-raise-on-partial-outputs" \
   --input "LSSTCam/defaults,LSSTCam/templates,LSSTCam/prompt/output-${DATE}" \
   --output "$OUTPUT_COLLECTION" \
